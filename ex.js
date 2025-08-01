694458400c5a03285f1d4e1b1cb821a98fbaf598
alishbaamin518 <alishbaamin518@gmail.com>
2025-08-02 04:46:32 +0500

    first commit

---
+++ ex.js
@@ -0,0 +1,198 @@
+// //_______________________________chapter 1________________________________________________
+// //-----------------Question 1,2----------------------
+// alert("Error! please entre a valid password");
+// //-----------------Question 3---------------------
+// alert("Welcome To JS Land...\nHappy Coding");
+// //-----------------Question 4---------------------
+// alert("Welcome To JS Land");
+// alert("Happy Coding!\n⬜ Prevent this page form creating additional dialogs.");;
+// //-----------------Question 5---------------------
+// alert("Hello!I can run JS through my web browser's console");
+// //-----------------Question 6---------------------
+// //-----------------Question 7---------------------
+//     //_______________________________chapter 2_____________________________________________
+// //-----------------Question 1---------------------
+// var username ='alishba'
+// //-----------------Question 2---------------------
+// var myname ='Alishba'
+// var father =' M.Amin'
+// var fullname = myname + father
+// console.log(fullname)
+// //-----------------Question 3---------------------
+// var message = 'HELLO WORLD'
+// alert(message)
+// //-----------------Question 4---------------------
+// var studentName = 'Hina'
+// var studentAge = '20 years old'
+// var studentEdu = 'Certified Mobile App Development'
+// alert(studentName);
+// alert(studentAge);
+// alert(studentEdu);
+// //-----------------Question 5---------------------
+// var pizza = 'PIZZA\nPIZZ\nPIZ\nPI\nP';
+// alert(pizza);
+// //-----------------Question 6---------------------
+// var email = 'alishbaamin518@gmail.com'
+// alert("My Email address is " + email)
+// //-----------------Question 7---------------------
+// var book = 'A Smarter Way To Learn JavaScript'
+// alert("I am trying to learn from the " + " BOOK " + book)
+//-----------------Question 8---------------------
+document.write("<h1> Yah!! I can write HTML through JavaScript</h1>")
+//-----------------Question 9---------------------
+var string ="▬▬▬▬▬▬◦•●◉✿ 🅐🅛🅘🅢🅗🅑🅐 🅐🅜🅘🅝 ✿◉●•◦▬▬▬▬▬▬ <br>"
+alert(string);
+document.write(string);
+ //_______________________________chapter 3_____________________________________________
+//-----------------Question 1---------------------
+var age ="i am 22 years old"
+alert(age);
+//-----------------Question 2---------------------
+var visitTime ="You have visited this websites 18 times"
+alert(visitTime);
+//-----------------Question 3---------------------
+var birthYear = "My birth year is ";
+var num =2004
+document.write(birthYear + num);
+//-----------------Question 4---------------------
+var VisitorsName = "<b><br>Fizza Rehman</b>";
+var ProductTitle ="<b> T-shirt </b>(S)"
+var Quantity ="<b> 5 </b>"
+document.write(VisitorsName + " ordered " + Quantity + ProductTitle + " on XYZ Clothing brand ")
+ //_______________________________chapter 4_____________________________________________
+//-----------------Question 1---------------------
+// 3 var in one statement
+var a ="Green" , b ="Pink" , c ="Brown";
+console.log(a , b , c) 
+//-----------------Question 2---------------------
+                  //5 legal variable
+  //1- number beech or last mai ================  var = n2um3
+  //2- under score start centre end mai laga saktain hai ======== var =  _set_a_password_ 
+  //3- $dollor sign bhi kahin bhi ==========  var = $hello$everyone
+  //4- camrl case use karai per sentance ka first word small or baku first word capital               =========== var = doHomeWork
+  //5- pascal case all are capital ======= var = MY_NUMBER 
+                    //5 illegal variable
+  //1- number se start nhi kar saktain 
+  //2- or sirf number bhi nhi hoga 
+  //3- space nhi de saktain variable mai
+  //4- special  charector nhi lag saktain siwaye kuch ke
+  //5-  "string" bhi nhi lag sakta            
+//-----------------Question 3---------------------
+
+//-----------------Question 4---------------------
+
+//-----------------Question 5---------------------
+
+//-----------------Question 6---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 1---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 1---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 1---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 1---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 1---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 1---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 1---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 1---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 1---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 1---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 1---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 1---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 1---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 1---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 1---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 1---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 1---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 1---------------------
+
+//-----------------Question 2---------------------
+
+//-----------------Question 2---------------------
